8336cfbcdeb177d8d909cb50d7f20d28507613b4
Dave Harned <dharned@gmail.com>
2016-08-25 22:00:12 -0500

    format views

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/jamesbondDB');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make DB accessible to router
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
